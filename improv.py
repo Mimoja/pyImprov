Mimoja/pyImprov

commit 6dc3a2ce26ece5c82301fa4c11131813d7355b37

Segment Response if not fitting the message
Improv messages use a single byte as length field.
It therefore defines an upper limit to the messages length.
Furthermore BLE implementations might only support limited
amounts of bytes (e.g. 20 or 50).
Allow for message segementation to respect those limits.

Signed-off-by: Mimoja <git@mimoja.de>

improv.py
@@ -99,6 +99,11 @@ class ImprovProtocol:
             return (ImprovCommand.UNKNOWN,)
 
         logging.info(f"Command recieved: {command}")
+        if len(data) == 1:
+            if command == ImprovCommand.WIFI_SETTINGS:
+                logger.warning("WIFI settings command without payload")
+                return (ImprovCommand.UNKNOWN,)
+            return (command,)
         length = data[1]
 
         if (length != len(data) - 3):
@@ -125,44 +130,57 @@ class ImprovProtocol:
             password_start = ssid_end + 1
             if password_start + password_length >= len(data):
                 return (command,)
-            password = bytearray(data[password_start: password_start + password_length])
+            password = bytearray(
+                data[password_start: password_start + password_length])
 
             return (command, ssid, password)
 
         return (command,)
 
-    def build_rpc_response(command: ImprovCommand, data: list[str]) -> bytearray:
+    def build_rpc_response(self, command: ImprovCommand, data: list[str]) -> bytearray:
         """Builds an bytearray from an command and data to be passed to the caller
 
         Args:
             command (ImprovCommand): The RPC command this is answering
             data (list[str]): data to be passed to the caller, e.g. redirect urls 
-            
+
         Returns:
             bytearray: Formated bytearray with length and checksum fields
         """
-        response = bytearray()
-        response += command.value.to_bytes(1,'little')
+        responses = []
+        current_response = bytearray()
+        current_response += command.value.to_bytes(1, 'little')
         # Leave space for length field
-        response += b"\x00"
-        for url in data:
-            response += len(url).to_bytes(1,'little')
-            response += url.encode("utf-8")
+        current_response += b"\x00"
+        for component in data:
+            if len(current_response) - 2 + 1 + len(component) > self.max_response_bytes:
+                current_response[1] = len(current_response) - 2
+                current_response += self.calculateChecksum(
+                    current_response).to_bytes(1, 'little')
+                # Add finished response to answer field
+                if len(current_response) <= self.max_response_bytes:
+                    responses.append(current_response)
+                # Create new response
+                current_response = bytearray()
+                current_response += command.value.to_bytes(1, 'little')
+                # Leave space for length field
+                current_response += b"\x00"
 
-        response[1] = len(response) - 2
-        response += ImprovProtocol.calculateChecksum(response).to_bytes(1,'little')
-        return response
+            current_response += len(component).to_bytes(1, 'little')
+            current_response += component.encode("utf-8")
+
+        return responses
 
     def handle_read(self, uuid: str) -> bytearray:
         match uuid:
             case ImprovUUID.STATUS_UUID.value:
-                return bytearray(self.state.value.to_bytes(1,'little'))
+                return bytearray(self.state.value.to_bytes(1, 'little'))
             case ImprovUUID.CAPABILITIES_UUID.value:
                 if self.identify_callback != None:
                     return bytearray([0x01])
                 return bytearray([0x01])
             case ImprovUUID.ERROR_UUID.value:
-                return bytearray(self.last_error.value.to_bytes(1,'little'))
+                return bytearray(self.last_error.value.to_bytes(1, 'little'))
             case ImprovUUID.RPC_RESULT_UUID.value:
                 return self.rpc_response
             case _:

commit 7e65bc30f7b9b34a2a014d9a476b78d9bd7388c8

Fixup: append the last rpc reponse to the responses buffer as well
The last reponse was never added to the trasmission stack,
causing it to never be transmitted to the caller.

Fixes: 6dc3a2c  Segment Response if not fitting the message

Signed-off-by: Mimoja <git@mimoja.de>

improv.py
@@ -63,7 +63,7 @@ class ImprovProtocol:
                  identify_callbackindentify_callback=None,
                  wifi_networks_callback=None,
                  device_info_callback=None,
-                 max_response_bytes=20,
+                 max_response_bytes=100,
                  ) -> None:
         self.requires_authorization = requires_authorization
         if requires_authorization:
@@ -169,6 +169,10 @@ class ImprovProtocol:
             current_response += len(component).to_bytes(1, 'little')
             current_response += component.encode("utf-8")
 
+        current_response[1] = len(current_response) - 2
+        current_response += self.calculateChecksum(
+            current_response).to_bytes(1, 'little')
+        responses.append(current_response)
         return responses
 
     def handle_read(self, uuid: str) -> bytearray:
@@ -235,7 +239,7 @@ class ImprovProtocol:
                             else:
                                 self.last_error = ImprovError.UNKNOWN
                         else:
-                            print(
+                            logger.warning(
                                 "Client requested GET_DEVICE_INFO but it is not implemented")
                             self.last_error = ImprovError.UNKNOWN_RPC
                     case ImprovCommand.GET_WIFI_NETWORKS:
@@ -247,16 +251,16 @@ class ImprovProtocol:
                             else:
                                 self.last_error = ImprovError.UNKNOWN
                         else:
-                            print(
+                            logger.warning(
                                 "Client requested GET_WIFI_NETWORKS but it is not implemented")
                             self.last_error = ImprovError.UNKNOWN_RPC
                     case _:
                         self.last_error = ImprovError.UNKNOWN_RPC
                 if self.last_error != ImprovError.NONE:
-                    print(
+                    logger.warning(
                         f"An error occured during execution: {self.last_error}")
                     return (ImprovUUID.ERROR_UUID.value, bytearray(self.last_error.value.to_bytes(1, 'little')))
-                print(f"RPC response: {self.rpc_response}")
+                logger.warning(f"RPC response: {self.rpc_response}")
                 return (ImprovUUID.RPC_RESULT_UUID.value, self.rpc_response)
             # Not our UUID
             case _:

mini-selftest.py
@@ -4,13 +4,18 @@
 
 from improv import *
 import logging
+from typing import Optional
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 logger = logging.getLogger(name=__name__)
 
-improv_server = ImprovProtocol(wifi_connect_callback=None)
+
+def wifi_connect_callback(ssid: str, passwd: str) -> Optional[list[str]]:
+    return ["http://SucessfullConnect.com"]
+
+improv_server = ImprovProtocol(wifi_connect_callback=wifi_connect_callback)
 
 
 def selftest():
@@ -31,14 +36,15 @@ def selftest():
         except TypeError as e:
             improv_data += component.to_bytes()
     improv_data[1] = len(improv_data) - 2
-    improv_data += ImprovProtocol.calculateChecksum(improv_data).to_bytes()
+    improv_data += improv_server.calculateChecksum(improv_data).to_bytes()
     logging.debug(improv_data)
     logging.debug(improv_server.handle_write(
         ImprovUUID.RPC_COMMAND_UUID.value, improv_data))
 
     rpc_urls = ["http://my-meticulous.local", "http://dummy"]
-    logging.debug(ImprovProtocol.build_rpc_response(
-        ImprovCommand.WIFI_SETTINGS, rpc_urls))
+    response= improv_server.build_rpc_response(
+        ImprovCommand.WIFI_SETTINGS, rpc_urls)
+    logging.debug(response)
 
     ssid = b"1234"
     password = b"5678"
@@ -51,9 +57,9 @@ def selftest():
         except TypeError as e:
             improv_data += component.to_bytes()
     improv_data[1] = len(improv_data) - 2
-    improv_data += ImprovProtocol.calculateChecksum(improv_data).to_bytes()
+    improv_data += improv_server.calculateChecksum(improv_data).to_bytes()
     logging.debug(improv_data)
-    failed_parse = ImprovProtocol.parse_improv_data(improv_data)
+    failed_parse = improv_server.parse_improv_data(improv_data)
     logging.debug(failed_parse)
     assert (len(failed_parse) == 1)
 